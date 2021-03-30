a9acf110801e54eb3697da780bd845db316ff3fd
root <root@zoo.c.qoo-project.internal>
2021-03-30 03:35:41 +0000

    rm /sys/fs/cgroup/cpuacct,cpu

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 
-mount -o remount,rw '/sys/fs/cgroup'
-ln -s /sys/fs/cgroup/cpu,cpuacct /sys/fs/cgroup/cpuacct,cpu
+sudo mount -o remount,rw '/sys/fs/cgroup'
+sudo rm -rf '/sys/fs/cgroup/cpuacct,cpu'
+sudo ln -s '/sys/fs/cgroup/cpu,cpuacct' '/sys/fs/cgroup/cpuacct,cpu'
 
 yum install docker -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&  sudo chmod +x /usr/local/bin/docker-compose  && docker-compose --version && systemctl restart docker && systemctl enable  docker && systemctl stop postfix && systemctl disable postfix
